0645e5739a1aac4e1ba03c6565b15f2f0e6dd45e
ahsanz1 <ahsanz639@gmail.com>
2024-07-31 14:37:23 +0500

    feat: added status route

--- proxyServer.js
+++ proxyServer.js
@@ -1,9 +1,13 @@
 const proxy = require("express-http-proxy");
 const express = require("express");
 const app = express();
-const PORT = 8000;
+const PORT = 80;
 
 const API_BASE_URL = "https://api.uat.equifax.co.uk";
+
+app.get("/status", (req, res) => {
+  res.send("Proxy server is running smoothly!");
+});
 
 app.use("/", proxy(API_BASE_URL));
 
