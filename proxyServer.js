5fd4a0d24cfcf49f3727bcc94e5e92e45d1441d9
ahsanz1 <ahsanz639@gmail.com>
2024-08-08 12:35:13 +0500

    feat: request logged

--- proxyServer.js
+++ proxyServer.js
@@ -9,7 +9,15 @@ app.get("/status", (req, res) => {
   res.send("Proxy server is running smoothly!");
 });
 
-app.use("/", proxy(API_BASE_URL));
+app.use(
+  "/",
+  proxy(API_BASE_URL, {
+    filter: (req, res) => {
+      console.log("Request: ", req.body, req.params);
+      return true;
+    },
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
